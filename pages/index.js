b54b5e6cdc1840481a13fdb7981efb16b485e10d
Luiz Lima (CTW) <Luiz.Lima@ctw.bmwgroup.com>
2024-08-11 14:25:45 +0100

    add image for test

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Test 1</h1>;
+    return (
+        <div>
+            <h1>Updating page view for test</h1>
+            <img
+                alt='haku'
+                src='https://soranews24.com/wp-content/uploads/sites/3/2022/01/Spirited-Away-fan-theory-Studio-Ghibli-anime-movie-Japan-film-news-Hayao-Miyazaki-Chihiro-Haku-4.jpg?w=1200&h=630&crop=1'
+            />
+        </div>
+    );
 }
 
 export default Home;
