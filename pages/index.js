06dbdb06531ecaebd91efd4672638df7222ad950
Luiz Lima (CTW) <Luiz.Lima@ctw.bmwgroup.com>
2024-08-12 21:50:44 +0100

    add script `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
 function Home() {
-    return (
-        <div>
-            <h1>Updating page view for test</h1>
-            <img
-                alt='haku'
-                src='https://soranews24.com/wp-content/uploads/sites/3/2022/01/Spirited-Away-fan-theory-Studio-Ghibli-anime-movie-Japan-film-news-Hayao-Miyazaki-Chihiro-Haku-4.jpg?w=1200&h=630&crop=1'
-            />
-        </div>
-    );
+  return (
+    <div>
+      <h1>Updating page view for test</h1>
+      <img
+        alt="haku"
+        src="https://soranews24.com/wp-content/uploads/sites/3/2022/01/Spirited-Away-fan-theory-Studio-Ghibli-anime-movie-Japan-film-news-Hayao-Miyazaki-Chihiro-Haku-4.jpg?w=1200&h=630&crop=1"
+      />
+    </div>
+  );
 }
-
 export default Home;
